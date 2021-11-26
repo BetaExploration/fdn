92fd3ef400d4026041fe3527674323a15082b8fd
axantillon <aantillon.leiva@gmail.com>
2021-11-25 18:34:14 -0600

    use react-modal + my modal logic

--- src/components/base/modal.tsx
+++ src/components/base/modal.tsx
@@ -1,11 +1,32 @@
 import { Box, Stack, Text } from "degen";
 import React from "react";
+import ReactModal from 'react-modal'
 
 export default function Modal ({wrapper, isOpen, children}) {
 
     return (
-        <>{isOpen && 
-            <Box position="absolute" top="0" left="0" width="viewWidth" height="viewHeight" display="flex" justifyContent="center" alignItems="center" backgroundColor="foregroundSecondary" style={{opacity: "10"}}>
+        <>
+            <ReactModal isOpen={isOpen} preventScroll={true} style={{
+                overlay: {
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundColor: 'rgba(0, 0,0, 0.8)'
+                },
+                content: {
+                    position: 'absolute',
+                    display: 'flex',
+                    flex: 1,
+                    justifyContent: 'center',
+                    alighItems: 'center',
+                    overflow: 'auto',
+                    WebkitOverflowScrolling: 'touch',
+                    border: 'none',
+                    backgroundColor: 'transparent',
+                }
+            }}>
                 <Box ref={wrapper} display="flex" maxWidth="1/2" backgroundColor="background" borderRadius="large" padding="12">
                     <Stack>
                         { children ? children :
@@ -17,7 +38,7 @@ export default function Modal ({wrapper, isOpen, children}) {
                         }
                     </Stack>
                 </Box>
-            </Box>  
-        }</>
+            </ReactModal>  
+        </>
     )
 }
